d195c52d21f96740237917832232d0fb9c01859a
GolddolphinSKB-Fexcraftian <games.tudorrose@gmail.com>
2018-12-27 16:44:25 +0000

    Changed colour order, again

--- Animator.py
+++ Animator.py
@@ -1,9 +1,6 @@
 import time
 from dotstar import Adafruit_DotStar
 
-def Colour(red, green, blue, white=0):
-    return int((white * pow(2, 24)) + (blue * pow(2, 16)) + (green * pow(2, 8)) + red)
-
 class Animation:
     def __init__(self, numpixels, delay_time=0.02, brightness=16):
         self.numpixels = numpixels
@@ -17,8 +14,7 @@ class Animation:
     def animateFromArray(self, data):
         for pixelNum in range(len(data)):
             print([255, data[pixelNum] * 255, data[pixelNum] * 255])
-            pixelColor = Colour(255, data[pixelNum] * 255, data[pixelNum] * 255)
-            self.strip.setPixelColor(pixelNum, pixelColor)
+            self.strip.setPixelColor(pixelNum, 255, data[pixelNum] * 255, data[pixelNum] * 255)
             self.strip.show()
             time.sleep(self.delay_time)
     
